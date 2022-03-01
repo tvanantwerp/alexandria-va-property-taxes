8e5bb4730b4742fe067d511dbdc401f57a46e6a4
Tom VanAntwerp <tom@tomvanantwerp.com>
2022-03-01 13:24:29 -0500

    Strip out this frustrating nonsense

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import cheerio, { CheerioAPI } from 'cheerio';
-import { csvParse } from 'd3-dsv';
 import * as fs from 'fs';
 import * as path from 'path';
 import { fileURLToPath } from 'url';
 
+import { streets } from './streets';
+
 interface Address {
-  streetNumber: number;
+  streetNumber?: number;
   streetName: string;
 }
 
@@ -18,27 +18,16 @@ function getPropertyURI({ streetNumber, streetName }: Address) {
 
 async function fetchPageData(URI: string) {
   const HTMLData = await axios.get(URI);
-  const $ = cheerio.load(HTMLData.data);
-  return $;
+  return HTMLData.data;
 }
 
-async function parsePageData($: CheerioAPI, firstPage = true) {
-  const pages = $('center p a');
-  if (pages.length > 2 && firstPage) {
-    const URIs = [...new Set(pages.map((i, el) => $(el).attr('href')))];
-    URIs.splice(URIs.findIndex(text => text.includes('CPage=0')));
-    console.log(URIs);
-    const allParsedPages: string[][] = await Promise.all(
-      URIs.map(async URI => {
-        const $$ = await fetchPageData(URI);
-        return parsePageData($$, false);
-      }),
-    );
-    return allParsedPages.flat();
-  }
-  return $('.searchResultDetailRow td:nth-of-type(3) span:nth-of-type(2)')
-    .map((i, el) => $(el).text())
-    .toArray();
+async function parsePageData(data: string, firstPage = true) {
+  // select pages with 'center p a
+  // if (pages.length > 2 && firstPage) {
+  //create URI array, then call fetchPageData for them
+  // }
+  //return parsed data on account
+  return 'hi';
 }
 
 async function getAccountNumbers(streetAddresses: Address[]) {
@@ -50,23 +39,7 @@ async function getAccountNumbers(streetAddresses: Address[]) {
   );
 }
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-fs.readFile(
-  path.join(__dirname, '../../../data/test.csv'),
-  'utf-8',
-  async (err, data) => {
-    if (err) console.error(err);
-
-    const streetAddresses = csvParse(data).map(address => {
-      return {
-        streetNumber: +address.STNUM,
-        streetName: [address.STPFX, address.STNAME, address.STTYPE].join('^'),
-      };
-    });
-
-    const accounts = await getAccountNumbers(streetAddresses);
-    console.log(accounts);
-  },
-);
+async function getData() {
+  const accounts = await getAccountNumbers(streets);
+  console.log(accounts);
+}
