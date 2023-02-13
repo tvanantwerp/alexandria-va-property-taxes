80ae3e6f59c6c7246b9e5df7eaabacd6203db972
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-02-12 21:41:11 -0500

    Rewrite accounts for Deno

--- scraper/accounts.ts
+++ scraper/accounts.ts
@@ -1,72 +1,67 @@
-import { PromisePool } from '@supercharge/promise-pool';
+import { pooledMap } from 'std/async/pool.ts';
 
-import { streets } from './streets';
+import { streets } from './streets.ts';
 import {
-  Address,
-  BASE_URL,
-  fetchPageData,
-  getPropertyURI,
-  sleep,
-} from './util';
+	Address,
+	BASE_URL,
+	fetchPageData,
+	getPropertyURI,
+	sleep,
+} from './util.ts';
 
 async function parsePageData(
-  data: Document,
-  firstPage = true,
+	data: Document,
+	firstPage = true,
 ): Promise<string[]> {
-  const pageLinks: HTMLAnchorElement[] = Array.from(
-    data.querySelectorAll('center p a'),
-  );
-  if (pageLinks.length > 2 && firstPage) {
-    let pages: string[] = [];
-    pageLinks.forEach(link => {
-      const href = link.href;
-      if (!pages.includes(href)) {
-        pages.push(href);
-      }
-    });
-    pages = pages
-      .filter(link => !link.includes('&CPage=0') && !link.includes('%5'))
-      .sort();
+	const pageLinks: HTMLAnchorElement[] = Array.from(
+		data.querySelectorAll('center p a'),
+	);
+	if (pageLinks.length > 2 && firstPage) {
+		let pages: string[] = [];
+		pageLinks.forEach((link) => {
+			const href = link.href;
+			if (!pages.includes(href)) {
+				pages.push(href);
+			}
+		});
+		pages = pages
+			.filter((link) => !link.includes('&CPage=0') && !link.includes('%5'))
+			.sort();
 
-    const allPages = await Promise.all(
-      pages.map(async page => {
-        const rawData = await fetchPageData(`${BASE_URL}${page}`);
-        return parsePageData(rawData, false);
-      }),
-    );
+		const allPages = await Promise.all(
+			pages.map(async (page) => {
+				const rawData = await fetchPageData(`${BASE_URL}${page}`);
+				return parsePageData(rawData, false);
+			}),
+		);
 
-    return allPages.flat();
-  }
+		return allPages.flat();
+	}
 
-  return Array.from(
-    data.querySelectorAll(
-      '.searchResultDetailRow > td:nth-child(3) > span:nth-child(2)',
-    ),
-  ).map(el => el.innerHTML);
+	return Array.from(
+		data.querySelectorAll(
+			'.searchResultDetailRow > td:nth-child(3) > span:nth-child(2)',
+		),
+	).map((el) => el.innerHTML);
 }
 
 async function getRawAccounts(streets: Address[]) {
-  const { results, errors } = await PromisePool.withConcurrency(5)
-    .for(streets)
-    .handleError(async (error, street, pool) => {
-      if (error) {
-        console.error('Error in handling street.', error, street);
-        return pool.stop();
-      }
-    })
-    .process(async street => {
-      const rawData = await fetchPageData(getPropertyURI(street));
-      sleep(100);
-      return parsePageData(rawData);
-    });
-  if (errors) {
-    console.error('Failure in getRawAccounts', errors);
-  }
-  return results;
+	const accountsData = await pooledMap(5, streets, async (street: Address) => {
+		const rawData = await fetchPageData(getPropertyURI(street));
+		sleep(100);
+		return parsePageData(rawData);
+	});
+
+	let accounts: string[] = [];
+	for await (const account of accountsData) {
+		accounts.push(account);
+	}
+
+	return accounts;
 }
 
 export async function getAccountNumbers() {
-  const accounts = await getRawAccounts(streets);
-  const flatAccounts = accounts.flat();
-  return flatAccounts;
+	const accounts = await getRawAccounts(streets);
+	const flatAccounts = accounts.flat();
+	return flatAccounts;
 }
