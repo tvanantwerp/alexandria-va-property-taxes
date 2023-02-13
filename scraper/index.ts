8ebb0e1aafbcd4668fd14f8b716829d7e3411b40
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-02-12 21:24:38 -0500

    Rewrite index and util to Deno

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,108 +1,97 @@
-import { PromisePool } from '@supercharge/promise-pool';
-import { existsSync, readFileSync, writeFile } from 'fs';
-import { resolve } from 'path';
+import { ensureDirSync, ensureFileSync } from 'std/fs/mod.ts';
+import { dirname, resolve } from 'std/path/mod.ts';
+import { pooledMap } from 'std/async/pool.ts';
 
-import { getAccountNumbers } from './accounts';
-import { parsePropertyDetails } from './properties';
+import { getAccountNumbers } from './accounts.ts';
+import { parsePropertyDetails, Property } from './properties.ts';
 
 async function getProperties(accounts: string[]) {
-  let count = 1;
-  const studyGroups: { [index: number]: number[] } = {};
-  const propertyTypes: { [index: string]: number } = {};
-  const { results, errors } = await PromisePool.withConcurrency(5)
-    .for(accounts)
-    .handleError(async (error, account, pool) => {
-      if (error) {
-        console.error(
-          'Experienced an error while getting properties.',
-          error,
-          account,
-        );
-        return pool.stop();
-      }
-    })
-    .process(async account => {
-      console.log(
-        `Fetching account ${count} of ${accounts.length}, number ${account}.`,
-      );
-      count++;
-      const property = await parsePropertyDetails(account);
-      if (property) {
-        if (property.studyGroup in studyGroups) {
-          studyGroups[property.studyGroup].push(+account);
-        } else {
-          studyGroups[property.studyGroup] = [+account];
-        }
-        if (property.type in propertyTypes) {
-          propertyTypes[property.type] += 1;
-        } else {
-          propertyTypes[property.type] = 1;
-        }
-        return property;
-      }
-    });
-  if (errors.length) {
-    console.error('Failure in getProperties', errors);
-  }
-  const formattedStudyGroups: { group: number; accounts: number[] }[] = [];
-  for (const [key, value] of Object.entries(studyGroups)) {
-    formattedStudyGroups.push({ group: +key, accounts: value });
-  }
-  return [
-    results.filter(result => result !== null),
-    formattedStudyGroups,
-    propertyTypes,
-  ];
+	let count = 1;
+	const studyGroups: { [index: number]: number[] } = {};
+	const propertyTypes: { [index: string]: number } = {};
+	const propertyDetails = pooledMap(5, accounts, async (account: string) => {
+		console.log(
+			`Fetching account ${count} of ${accounts.length}, number ${account}.`,
+		);
+		count++;
+		const property = await parsePropertyDetails(account);
+		if (property) {
+			if (property.studyGroup in studyGroups) {
+				studyGroups[property.studyGroup].push(+account);
+			} else {
+				studyGroups[property.studyGroup] = [+account];
+			}
+			if (property.type in propertyTypes) {
+				propertyTypes[property.type] += 1;
+			} else {
+				propertyTypes[property.type] = 1;
+			}
+			return property;
+		}
+	});
+
+	let results: Property[] = [];
+	for await (const property of propertyDetails) {
+		if (property) {
+			results.push(property);
+		}
+	}
+
+	const formattedStudyGroups: { group: number; accounts: number[] }[] = [];
+	for (const [key, value] of Object.entries(studyGroups)) {
+		formattedStudyGroups.push({ group: +key, accounts: value });
+	}
+
+	return [
+		results,
+		formattedStudyGroups,
+		propertyTypes,
+	];
 }
 
 async function getAssessments() {
-  let accounts: string[];
-  if (existsSync(resolve(__dirname, `../../../data/accounts.json`))) {
-    console.log('Getting list of accounts from file...');
-    accounts = JSON.parse(
-      readFileSync(resolve(__dirname, `../../../data/accounts.json`), {
-        encoding: 'utf8',
-      }),
-    );
-  } else {
-    console.log('Scraping to get list of accounts...');
-    accounts = await getAccountNumbers();
+	ensureDirSync('../.cache');
+	let accounts: string[];
+	ensureFileSync(resolve(dirname, `../../../data/accounts.json`));
+	const accountsFile = Deno.readTextFileSync(
+		resolve(dirname, `../../../data/accounts.json`),
+	);
+	if (accountsFile.length > 0) {
+		console.log('Getting list of accounts from file...');
+		accounts = JSON.parse(
+			Deno.readTextFileSync(resolve(dirname, `../../../data/accounts.json`)),
+		);
+	} else {
+		console.log('Scraping to get list of accounts...');
+		accounts = await getAccountNumbers();
 
-    console.log('Saving scraped list of accounts...');
-    writeFile(
-      resolve(__dirname, '../../../data/accounts.json'),
-      JSON.stringify(accounts),
-      { encoding: 'utf8' },
-      err => console.error(err),
-    );
-  }
+		console.log('Saving scraped list of accounts...');
+		Deno.writeTextFile(
+			resolve(dirname, '../../../data/accounts.json'),
+			JSON.stringify(accounts),
+		);
+	}
 
-  const [properties, studyGroups, propertyTypes] = await getProperties(
-    accounts,
-  );
+	const [properties, studyGroups, propertyTypes] = await getProperties(
+		accounts,
+	);
 
-  console.log('Writing properties...');
-  writeFile(
-    resolve(__dirname, '../../../data/properties.json'),
-    JSON.stringify(properties),
-    { encoding: 'utf8' },
-    err => console.error(err),
-  );
-  console.log('Writing study groups...');
-  writeFile(
-    resolve(__dirname, '../../../data/groups.json'),
-    JSON.stringify(studyGroups),
-    { encoding: 'utf8' },
-    err => console.error(err),
-  );
-  console.log('Writing property types...');
-  writeFile(
-    resolve(__dirname, '../../../data/types.json'),
-    JSON.stringify(propertyTypes),
-    { encoding: 'utf8' },
-    err => console.error(err),
-  );
-  console.log('Done!');
+	console.log('Writing properties...');
+	Deno.writeTextFile(
+		resolve(dirname, '../../../data/properties.json'),
+		JSON.stringify(properties),
+	);
+	console.log('Writing study groups...');
+	Deno.writeTextFile(
+		resolve(dirname, '../../../data/groups.json'),
+		JSON.stringify(studyGroups),
+	);
+	console.log('Writing property types...');
+	Deno.writeTextFile(
+		resolve(dirname, '../../../data/types.json'),
+		JSON.stringify(propertyTypes),
+	);
+	console.log('Done!');
 }
 
 getAssessments();
--- scraper/util.ts
+++ scraper/util.ts
@@ -1,10 +1,11 @@
-import {ensureFileSync} from 'std/fs/ensure_file.ts';
-import {DOMParser} from 'deno_dom';
-import * as path from 'path';
+import { ensureFileSync } from 'std/fs/ensure_file.ts';
+import { DOMParser } from 'deno_dom';
+import { dirname, resolve } from 'std/path/mod.ts';
+import { decode, encode } from 'std/encoding/base64.ts';
 
 export interface Address {
-  streetNumber?: string;
-  streetName: string;
+	streetNumber?: string;
+	streetName: string;
 }
 
 let count = 0;
@@ -12,49 +13,47 @@ let count = 0;
 export const BASE_URL = 'https://realestate.alexandriava.gov/';
 
 export function getPropertyURI({ streetNumber = '', streetName }: Address) {
-  return `${BASE_URL}index.php?StreetNumber=${streetNumber}&StreetName=${streetName}&UnitNo=&Search=Search`;
+	return `${BASE_URL}index.php?StreetNumber=${streetNumber}&StreetName=${streetName}&UnitNo=&Search=Search`;
 }
 
 export async function fetchPageData(URI: string, ignoreCache = false) {
-  console.log(`Getting data for ${URI}...`);
-  if (
-    !ignoreCache &&
-    ensureFileSync(
-      path.resolve(
-        __dirname,
-        `../../../.cache/${Buffer.from(URI).toString('base64')}.txt`,
-      ),
-    )
-  ) {
-    console.log(`I read ${URI} from cache`);
-    const data = await readFile(
-      path.resolve(
-        __dirname,
-        `../../../.cache/${Buffer.from(URI).toString('base64')}.txt`,
-      ),
-      { encoding: 'utf8' },
-    );
-    const dom = new JSDOM(data);
-    return dom.window.document;
-  } else {
-    count++;
-    console.log(`Fetch ${count}: I fetched ${URI} fresh`);
-    const HTMLData = await (await fetch(URI, {keepalive: true})).text();
-    if (!ignoreCache) {
-      Deno.writeTextFileSync(
-        path.resolve(
-          __dirname,
-          `../../../.cache/${Buffer.from(URI).toString('base64')}.txt`,
-        ),
-        HTMLData.data,
-        { encoding: 'utf8' },
-      );
-    }
-    const dom = new DOMParser(HTMLData);
-    return dom.window.document;
-  }
+	console.log(`Getting data for ${URI}...`);
+	if (
+		!ignoreCache &&
+		ensureFileSync(
+			resolve(
+				dirname,
+				`../../../.cache/${decode(URI)}.txt`,
+			),
+		)
+	) {
+		console.log(`I read ${URI} from cache`);
+		const data = await Deno.readTextFile(
+			resolve(
+				dirname,
+				`../../../.cache/${decode(URI)}.txt`,
+			),
+		);
+		const dom = new DOMParser(data);
+		return dom.window.document;
+	} else {
+		count++;
+		console.log(`Fetch ${count}: I fetched ${URI} fresh`);
+		const HTMLData = await (await fetch(URI, { keepalive: true })).text();
+		if (!ignoreCache) {
+			Deno.writeTextFileSync(
+				resolve(
+					dirname,
+					`../../../.cache/${encode(URI)}.txt`,
+				),
+				HTMLData,
+			);
+		}
+		const dom = new DOMParser(HTMLData);
+		return dom.window.document;
+	}
 }
 
 export function sleep(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
