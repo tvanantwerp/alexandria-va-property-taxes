c5f77724a8efe088fb765490e890ff53019f239d
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-08-28 21:04:07 -0400

    If property undefined, don't load to DB

--- packages/database/prisma/loadData.ts
+++ packages/database/prisma/loadData.ts
@@ -16,61 +16,63 @@ async function getProperties(): Promise<Property[]> {
 
 async function createProperty(property: Property) {
   try {
-    await db.property.create({
-      data: {
-        account: property.account,
-        streetNumber: property.streetNumber,
-        streetName: property.streetName,
-        description: property.description,
-        lotSize: property.lotSize,
-        yearBuilt: property.yearBuilt,
-        buildingType: property.buildingType,
-        livingArea: property.livingArea,
-        totalBasement: property.totalBasement,
-        finishedBasement: property.finishedBasement,
-        fullBaths: property.fullBaths,
-        halfBaths: property.halfBaths,
-        StudyGroup: {
-          connectOrCreate: {
-            create: {
-              studyGroupID: property.studyGroup,
-            },
-            where: {
-              studyGroupID: property.studyGroup,
-            },
-          },
-        },
-        PropertyType: {
-          connectOrCreate: {
-            create: {
-              propertyTypeId: property.type,
-            },
-            where: {
-              propertyTypeId: property.type,
+    if (property) {
+      await db.property.create({
+        data: {
+          account: property.account,
+          streetNumber: property.streetNumber,
+          streetName: property.streetName,
+          description: property.description,
+          lotSize: property.lotSize,
+          yearBuilt: property.yearBuilt,
+          buildingType: property.buildingType,
+          livingArea: property.livingArea,
+          totalBasement: property.totalBasement,
+          finishedBasement: property.finishedBasement,
+          fullBaths: property.fullBaths,
+          halfBaths: property.halfBaths,
+          StudyGroup: {
+            connectOrCreate: {
+              create: {
+                studyGroupID: property.studyGroup,
+              },
+              where: {
+                studyGroupID: property.studyGroup,
+              },
             },
           },
-        },
-        assessments: {
-          create: property.assessments.map(
-            ({ month, year, land, building }) => {
-              return {
-                month,
-                year,
-                land,
-                building,
-              };
+          PropertyType: {
+            connectOrCreate: {
+              create: {
+                propertyTypeId: property.type,
+              },
+              where: {
+                propertyTypeId: property.type,
+              },
             },
-          ),
+          },
+          assessments: {
+            create: property.assessments.map(
+              ({ month, year, land, building }) => {
+                return {
+                  month,
+                  year,
+                  land,
+                  building,
+                };
+              },
+            ),
+          },
+          sales: {
+            create: property.sales.map(
+              ({ id, purchaseCode, price, day, month, year }) => {
+                return { saleId: id, purchaseCode, price, day, month, year };
+              },
+            ),
+          },
         },
-        sales: {
-          create: property.sales.map(
-            ({ id, purchaseCode, price, day, month, year }) => {
-              return { saleId: id, purchaseCode, price, day, month, year };
-            },
-          ),
-        },
-      },
-    });
+      });
+    }
   } catch (error) {
     console.error(error, property);
   }
