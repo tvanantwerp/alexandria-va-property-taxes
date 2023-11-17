c875d6bb098997e3a99213219e0b092c2ed0fe98
Tom VanAntwerp <tom@tomvanantwerp.com>
2023-11-16 21:54:24 -0500

    Fix stupid typo that made all the owner data wrong...

--- packages/scraper/src/properties.ts
+++ packages/scraper/src/properties.ts
@@ -224,7 +224,7 @@ export async function parsePropertyDetails(
 
   const result: Property = {
     account: +account,
-    owner: sales.length >= 1 ? sales[0].grantor : '',
+    owner: sales.length >= 1 ? sales[0].grantee : '',
     streetNumber,
     streetName,
     type,
